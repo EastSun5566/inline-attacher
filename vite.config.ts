4fc7b37a8c0a874d47d76e863d32957c47a3f327
Michael Wang <michael19920327@gmail.com>
2023-07-15 03:02:50 +0800

    chore: tweak vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+/* eslint-disable import/no-extraneous-dependencies */
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+import { name } from './package.json';
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
-      name: 'InlineAttachment',
-      fileName: 'inline-attachment',
+      name: name.replace(/-(.)/g, ($1) => $1.toUpperCase()).replace(/-/g, ''),
+      fileName: name,
     },
   },
-})
+});
